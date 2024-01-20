1bb7ef68757f75a066cad96c73103cd1d537c30b
Kurbonnazar <95540818+Kurbonnazar@users.noreply.github.com>
2024-01-20 10:54:27 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCzVACZVkvLz5IzVOc8oIZjJ-cFeYV3OvM",
+  authDomain: "spectagram-4849d.firebaseapp.com",
+  databaseURL: "https://spectagram-4849d-default-rtdb.firebaseio.com",
+  projectId: "spectagram-4849d",
+  storageBucket: "spectagram-4849d.appspot.com",
+  messagingSenderId: "227854853645",
+  appId: "1:227854853645:web:1bd87fb0edcfcb8ebcd744"
+};
